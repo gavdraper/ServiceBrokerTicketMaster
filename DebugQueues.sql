143a78a9a5895639e43095d8f1b8d1f036b374ca
Gavin Draper <gavdraper@gmail.com>
2012-05-28 09:22:37 +0100

    Renamed configure queues to debugQueues and removed broker setup scripts as that is stored in the DB project

---
+++ DebugQueues.sql
@@ -0,0 +1,6 @@
+SELECT * FROM sys.transmission_queue
+SELECT * FROM sys.conversation_endpoints
+SELECT * FROM ProcessPaymentTargetQueue
+SELECT * FROM ProcessPaymentInitiatorQueue
+SELECT * FROM PrintTargetQueue
+SELECT * FROM PrintInitiatorQueue
